875cad4ec03647fb838281db3955efd512c3da66
Xwilarg <xwilarg@yahoo.fr>
2019-04-02 16:30:01 +0200

    Add endpoints

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Example of implementation of a WebServer in Go
 
 ## Endpoints
- - / (GET) Get all the colors store in the server (10x10 array, each color is in the format RRRGGGBBB)
- - / (POST) Set a new color (body argument must be [x position];[y position];[pixel color]
+ - /get (GET) Get all the colors store in the server (10x10 array, each color is in the format RRRGGGBBB)
+ - /update (POST) Set a new color (body argument must be [x position];[y position];[pixel color]
--- main.go
+++ main.go
@@ -11,20 +11,34 @@ import (
 
 var colors []string
 
-func getRequest(rw http.ResponseWriter, http *http.Request) {
+func setHeaders(rw http.ResponseWriter) {
 	rw.Header().Set("Access-Control-Allow-Origin", "*")
 	rw.Header().Set("Content-Type", "application/json")
-	switch http.Method {
+}
+
+func getColors(rw http.ResponseWriter, req *http.Request) {
+	setHeaders(rw)
+	switch req.Method {
 	case "GET":
 		fmt.Fprintf(rw, "[\"" + strings.Join(colors, "\",\"") + "\"]")
+	default:
+		rw.WriteHeader(405)
+	}
+}
+
+func updateColors(rw http.ResponseWriter, req *http.Request) {
+	setHeaders(rw)
+	switch req.Method {
 	case "POST":
 		buffer := new(bytes.Buffer)
-		buffer.ReadFrom(http.Body)
+		buffer.ReadFrom(req.Body)
 		arr := strings.Split(buffer.String(), ";")
 		x, _ := strconv.Atoi(arr[0])
 		y, _ := strconv.Atoi(arr[1])
 		colors[x * 10 + y] = arr[2]
 		fmt.Fprintf(rw, "[\"" + strings.Join(colors, "\",\"") + "\"]")
+	default:
+		rw.WriteHeader(405)
 	}
 }
 
@@ -33,7 +47,8 @@ func main() {
 	for i := 0; i < 100; i++ {
 		colors[i] = "255000000"
 	}
-	http.HandleFunc("/", getRequest)
+	http.HandleFunc("/get", getColors)
+	http.HandleFunc("/update", updateColors)
 	fmt.Printf("Listening...\nPress ^C to exit\n");
 	log.Fatal(http.ListenAndServe(":8082", nil))
 }
